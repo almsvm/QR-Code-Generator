2d125b557ee0abeca315232c5f4544291542e55f
Alime Sevim Ayhan <alimesevim@gmail.com>
2023-05-22 16:49:57 -0400

    qr code

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const form = document.getElementById('generate-form');
+const qr = document.getElementById('qrcode');
+
+// Button submit
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+
+  clearUI();
+
+  const url = document.getElementById('url').value;
+  const size = document.getElementById('size').value;
+
+  // Validate url
+  if (url === '') {
+    alert('Please enter a URL');
+  } else {
+    
+      generateQRCode(url, size);
+
+      // Generate the save button after the qr code image src is ready
+
+        // Get save url
+        const saveUrl = qr.querySelector('img').src;
+        // Create save button
+        createSaveBtn(saveUrl);
+      }
+    
+};
+
+
+// Generate QR code
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode('qrcode', {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+// Clear QR code and save button
+const clearUI = () => {
+  qr.innerHTML = '';
+  const saveBtn = document.getElementById('save-link');
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
+
+
+
+// Create save button to download QR code as image
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement('a');
+  link.id = 'save-link';
+  link.classList =
+    'bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 rounded w-2/3 m-auto my-5';
+  link.href = saveUrl;
+  link.download = 'qrcode';
+  link.innerHTML = 'Save Image';
+  document.getElementById('generated').appendChild(link);
+};
+
+
+form.addEventListener('submit', onGenerateSubmit);
